bc760ad2705942d19e4e8c4e5c00fb7e246607eb
Satoru Kobayashi <satoruk@icepp.s.u-tokyo.ac.jp>
2019-07-24 20:34:38 +0200

    import NetworkUtil

--- analysis/NetworkUtil.py
+++ analysis/NetworkUtil.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class ReducedConv(nn.Module):
     def __init__(self,input_size,output_size, input_dim, output_dim,kernel_size):
         super(ReducedConv, self).__init__()
