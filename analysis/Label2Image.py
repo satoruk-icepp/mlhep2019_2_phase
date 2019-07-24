58fb9d19a34d530841c5a2805000e609fa571e18
Satoru Kobayashi <satoruk@icepp.s.u-tokyo.ac.jp>
2019-07-24 20:53:18 +0200

    label 2 image

--- analysis/Label2Image.py
+++ analysis/Label2Image.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 def LabelToImages(row,col,MomentumPoint):
-    images = torch.zeros(MomentumPoint.shape()[0],MomentumPoint.shape()[1],row,col)
+    images = torch.zeros(MomentumPoint.shape[0],MomentumPoint.shape[1],row,col)
     for image,mp in zip(images,MomentumPoint):
         for i in range(MomentumPoint.shape()[1]):
             image[i,:,:]+=mp[i].cpu()
