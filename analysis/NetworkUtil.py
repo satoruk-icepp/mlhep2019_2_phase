7fe930e68a48de77975823603b0ca1c9fb246c86
Satoru Kobayashi <satoruk@icepp.s.u-tokyo.ac.jp>
2019-07-25 09:35:36 +0200

    bug fix

--- analysis/NetworkUtil.py
+++ analysis/NetworkUtil.py
@@ -3,14 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ReducedConv(nn.Module):
-    def __init__(self,input_size,output_size, input_dim, output_dim,kernel_size):
+    def __init__(self,input_layers,output_layers, input_dim, output_dim,kernel_size):
         super(ReducedConv, self).__init__()
         self.input_dim  = input_dim
         self.output_dim = output_dim
-        scale = float(output_dim+kernel_size-3)/float(input_dim)
+        scale = 2
+        self.kernel_size = self.input_dim*scale+ 3 - self.output_dim
+        # scale = float(output_dim+kernel_size-3)/float(input_dim)
         self.ups = nn.Upsample(scale_factor = scale,mode = 'bilinear',align_corners=False )
         self.ref = nn.ReflectionPad2d(1)
-        self.conv = nn.Conv2d(input_size,output_size,kernel_size)
+        self.conv = nn.Conv2d(input_layers,output_layers,self.kernel_size)
     def forward(self,x):
         assert x.shape[2]==self.input_dim, "original image is wrong size, %d"%(x.shape[2])
         x = self.conv(self.ref(self.ups(x)))
